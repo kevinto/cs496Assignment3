4e26c1ec054aa7eeae7aa54d3f978a7d8035e9eb
Kevin To <kevinto1989@gmail.com>
2015-11-29 03:02:21 +0000

    added password field to the user schema

--- model/user.js
+++ model/user.js
@@ -10,6 +10,10 @@ var UserSchema = new mongoose.Schema({
     unique: true,
     dropDups: true
   },
+  password: {
+    type: String,
+    required:true
+  },
   firstName: {
     type: String,
     required: true
